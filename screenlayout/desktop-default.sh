9b454d1d268c7e7f1caac4e64f388d58627c01d0
Erik Peyronson <erikpeyronson@gmail.com>
2016-11-01 20:37:29 +0100

    Added default desktop screenlayout

---
+++ screenlayout/desktop-default.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output DP1 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI3 --off --output HDMI2 --mode 1920x1080 --pos 1080x744 --rotate normal --output HDMI1 --mode 1280x1024 --pos 3000x744 --rotate normal --output VGA1 --off
