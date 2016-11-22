8693edb1a8f6a38fbd3acb0660b2b87da73c3c33
Erik Peyronson <erik@desktop.lan>
2016-11-22 17:39:13 +0100

    Fixed desktop screenlayout for new screen

--- screenlayout/desktop-default.sh
+++ screenlayout/desktop-default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP1 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI3 --off --output HDMI2 --mode 1920x1080 --pos 1080x744 --rotate normal --output HDMI1 --mode 1280x1024 --pos 3000x744 --rotate normal --output VGA1 --off
+xrandr --output VIRTUAL1 --off --output DP1 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI3 --off --output HDMI2 --primary --mode 1920x1080 --pos 1080x840 --rotate normal --output HDMI1 --mode 1920x1080 --pos 3000x840 --rotate normal --output VGA1 --off
