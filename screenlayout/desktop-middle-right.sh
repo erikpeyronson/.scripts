985adb32e8383ac4c5bc552460f6b99fc203a820
Erik Peyronson <erikpeyronson@gmail.com>
2017-01-04 10:42:57 +0100

    Renamed screenlayout scripts and added left-right.sh

---
+++ screenlayout/desktop-middle-right.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --primary --mode 1920x1080 --pos 1080x840 --rotate normal --output HDMI1 --mode 1920x1080 --pos 3000x840 --rotate normal --output VGA1 --off
